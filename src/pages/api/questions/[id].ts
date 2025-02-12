e610a0b2440fe444e4e791d62ae04480d4154088
Debora Borges <debora@lauduz.com>
2025-02-12 11:11:07 -0300

    filtrando perguntas pelo id requerido

--- src/pages/api/questions/[id].ts
+++ src/pages/api/questions/[id].ts
@@ -2,5 +2,15 @@ import questions from "../questionsDatabase";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-    res.status(200).json(questions[0].toObject());
+    const id = parseInt(req.query.id as string, 10);
+
+    const filteredQuestion = questions.filter(question => question.id === id)
+
+    if(filteredQuestion.length === 1){
+        const questionSelected = filteredQuestion[0]
+        return res.status(200).json(questionSelected.toObject());
+    } else{
+        return res.status(204).send("Nenhuma pergunta encontrada!")
+    }
+
 };
