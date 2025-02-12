788c41061e4becbada238ef704e136f7b92a5295
Debora Borges <debora@lauduz.com>
2025-02-12 14:06:00 -0300

    criação do componente de questão

---
+++ src/components/question/index.tsx
@@ -0,0 +1,15 @@
+import QuestionModel from "@/model/question";
+import styles from './style.module.css'
+interface QuestionProps {
+    value: QuestionModel
+}
+
+export default function Question(props: QuestionProps) {
+    const question = props.value
+
+    return(
+        <div className={styles.question}>
+            <h1>Question</h1>
+        </div>
+    )
+}
