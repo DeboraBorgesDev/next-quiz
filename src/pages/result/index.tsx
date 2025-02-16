5397e9ae23cbc51779b10579b05cc0b38d2f9544
Debora Borges <debora@lauduz.com>
2025-02-16 10:01:26 -0300

    criação do pa´gina  de resultado

--- src/pages/result/index.tsx
+++ src/pages/result/index.tsx
@@ -1,7 +1,29 @@
-export default function Result(){
+
+import { useRouter } from 'next/router'
+import styles from './styles.module.css'
+import Statistic from '@/components/statistic'
+import Button from '@/components/button'
+
+export default function Result() {
+    const router = useRouter()
+
+    const total = Number(router.query.total)
+    const corrects = Number(router.query.rightAnswers)
+    const percentual = Math.round((corrects/total) * 100)
+
+    console.log({total, corrects})
+
     return (
-        <div>
-            Resultado
+        <div className={styles.result}>
+            <h1>Resultado Final</h1>
+            <div style={{ display: "flex" }}>
+                <Statistic text="Perguntas" value={total} />
+                <Statistic text="Certas" value={corrects}
+                    backgroundColor="#9CD2A4"/>
+                <Statistic text="Percentual" value={`${percentual}%`}
+                    backgroundColor="#DE6A33" />
+            </div>
+            <Button href="/" text="Tentar Novamente" />
         </div>
     )
 }
